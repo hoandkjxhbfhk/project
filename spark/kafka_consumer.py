3552767c23969a2488471b560405db528b5cd341
hoandkjxhbfhk <hoanoc321@gmail.com>
2024-12-01 13:24:27 +0700

    lstm

--- spark/kafka_consumer.py
+++ spark/kafka_consumer.py
@@ -42,13 +42,13 @@ structured_data_stream = raw_data_stream.select(
 ).select("data.*")
 
 # Define the Delta Lake storage path
-delta_path = "../Time-Series-Forecasting-Spark-Kafka-for-Weather-Data-main/delta_lake/delta_lake_table_3"  # Replace with your actual Delta Lake storage path
+delta_path = "../Time-Series-Forecasting-Spark-Kafka-for-Weather-Data-main/delta_lake/delta_lake_table_4"  # Replace with your actual Delta Lake storage path
 
 # Write the streaming DataFrame to Delta Lake
 query = structured_data_stream.writeStream \
     .outputMode("append") \
     .format("delta") \
-    .option("checkpointLocation", "../Time-Series-Forecasting-Spark-Kafka-for-Weather-Data-main/delta_lake/checkpoint_3") \
+    .option("checkpointLocation", "../Time-Series-Forecasting-Spark-Kafka-for-Weather-Data-main/delta_lake/checkpoint_4") \
     .start(delta_path)
 
 # Await termination of the query
